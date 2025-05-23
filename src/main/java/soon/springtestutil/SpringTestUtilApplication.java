4f061474a7806d28710db4d4ec96d15011ff4c39
jjh75607 <dkrak5738@naver.com>
2025-05-23 19:35:24 +0900

    init

---
+++ src/main/java/soon/springtestutil/SpringTestUtilApplication.java
@@ -0,0 +1,13 @@
+package soon.springtestutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestUtilApplication.class, args);
+    }
+
+}
