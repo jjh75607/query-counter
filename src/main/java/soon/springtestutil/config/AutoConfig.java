0d6aa8c7f24a3d572d38d8da130ff6fae5d10f74
jjh75607 <dkrak5738@naver.com>
2025-06-04 16:50:16 +0900

    [#29] Setting: AutoConfig 추가

---
+++ src/main/java/soon/springtestutil/config/AutoConfig.java
@@ -0,0 +1,10 @@
+package soon.springtestutil.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "soon.springtestutil")
+public class AutoConfig {
+
+}
