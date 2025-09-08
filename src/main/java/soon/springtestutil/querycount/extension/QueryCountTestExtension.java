4ba5492f09aab013df3477a14a133bd20b60b1aa
JungSoonJae <34623431+jjh75607@users.noreply.github.com>
2025-09-08 17:31:19 +0900

    [#40] Fix: 각 테스트 실행 전후로 QueryCountContext를 초기화 (#41)

--- src/main/java/soon/springtestutil/querycount/extension/QueryCountTestExtension.java
+++ src/main/java/soon/springtestutil/querycount/extension/QueryCountTestExtension.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import soon.springtestutil.core.context.TestContextHolder;
+import soon.springtestutil.querycount.context.QueryCountContext;
 
 public class QueryCountTestExtension implements BeforeEachCallback, AfterEachCallback {
 
@@ -12,11 +13,15 @@ public class QueryCountTestExtension implements BeforeEachCallback, AfterEachCal
         String className = context.getRequiredTestClass().getName();
         String methodName = context.getRequiredTestMethod().getName();
         TestContextHolder.setContext(className, methodName);
+
+        QueryCountContext.clear();
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
         TestContextHolder.clearContext();
+
+        QueryCountContext.clear();
     }
 
 }
